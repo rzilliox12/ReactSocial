c1443c4544cffb8fcb4c278311609ffc866cbd22
Ray Zilliox <rzilliox@pls3rdlearning.com>
2021-04-15 17:38:52 -0400

    section 14 end of 162

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
+using Application.Profiles;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
         public Guid Id { get; set; }
-        
         public string Title { get; set; }   
         public DateTime Date { get; set; }  
         public string Description { get; set; } 
@@ -14,6 +14,8 @@ namespace Domain
         public string City { get; set; }    
         public string Venue { get; set; }
 
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
+        public string HostUsername { get; set; }
+
+        public ICollection<Profile> Profiles { get; set; }
     }
 }
